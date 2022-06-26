94f063c6815b5ecfda4622360bcc839e189a0f23
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2022-06-26 11:33:10 +0300

    add js

---
+++ resources/js/components/Posts/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <table class="table">
+      <thead>
+        <tr>
+            <th>Title</th>
+            <th>Post text</th>
+            <th>Created date</th>
+            <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+            <td>Something</td>
+            <td>Longer text</td>
+            <td>2020-01-23</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Something</td>
+            <td>Longer text</td>
+            <td>2020-01-23</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Something</td>
+            <td>Longer text</td>
+            <td>2020-01-23</td>
+            <td></td>
+        </tr>
+      </tbody>
+  </table>
+</template>
+
+<script>
+</script>
